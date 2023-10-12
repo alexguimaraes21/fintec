e0cd32a5557a6742f380167d56206b5cab5b8af4
Alex Carreira <rm97596@fiap.com.br>
2023-10-12 09:43:54 -0300

    -- Refeito o script de banco de dados do projeto Fintech;

--- fase6/cap6/Armazenando e manipulando informações no banco.sql
+++ fase6/cap6/Armazenando e manipulando informações no banco.sql
@@ -2,49 +2,38 @@
   Cadastro e alteração 
 */
 
-INSERT INTO T_TIPO_LANCAMENTO (cd_tipo, ds_tipo) VALUES (1, 'Crédito');
-INSERT INTO T_TIPO_LANCAMENTO (cd_tipo, ds_tipo) VALUES (2, 'Débito');
-INSERT INTO T_TIPO_LANCAMENTO (cd_tipo, ds_tipo) VALUES (3, 'CDC');
-
 -- Cadastrar os dados do usuário. 
-INSERT INTO T_USUARIO (cd_usuario, vl_cpf_usuario, ds_senha, nm_usuario, ck_usuario_ativo) 
-VALUES (1, '03901750053', '123mudar', 'João das Couves', 1);
+INSERT INTO T_USUARIO (cd_usuario, vl_cpf_usuario, ds_senha, nm_usuario, ck_usuario_ativo) VALUES ([CD USUARIO], '[CPF USUARIO]', '[SENHA USUARIO]', '[NOME USUARIO]', [VERDADEIRO]);
 
 -- Cadastrar os dados para a conta do usuário. 
-INSERT INTO T_CONTA (cd_conta, vl_agencia, vl_conta, vl_banco, cd_usuario) 
-VALUES (1, '3357-4', '00003574-3', '0033', 1);
+INSERT INTO T_CONTA (cd_conta, vl_agencia, vl_conta, vl_banco, cd_usuario) VALUES ([CD CONTA], '[AGENCIA E DIGITO]', '[CONTA E DIGITO]', '[COD BANCO]', [CD USUARIO]);
 
 -- Alterar todos os dados do usuário, utilizando seu código como referência. 
-UPDATE T_USUARIO SET nm_usuario = 'João das Couves da Silveira' WHERE cd_usuario = 1;
+UPDATE T_USUARIO SET nm_usuario = '[NOME USUARIO]' WHERE cd_usuario = [CD USUARIO];
+
+-- Inicializar tabela com os tipos dos lançamentos (RECEITA, DESPESA ou INVESTIMENTO).
+INSERT INTO T_TIPO_LANCAMENTO (cd_tipo_lancamento, ds_tipo_lancamento) VALUES ([CD TIPO LANCAMENTO], '[DESCR TIPO LANCAMENTO]')
+
+-- Inicializar tabela com as carteiras de investimentos
+INSERT INTO T_TIPO_INVESTIMENTO (cd_tipo_invesimento, ds_tipo_investimento, vl_rendimento_mensal, prazo_minino_investimento) VALUES ([CD TIPO INVESTIMENTO], '[DESCR TIPO INVESTIMENTO]', [VL DA % RENDIMENTO MENSAL], [PRAZO MININO INVESTIMENTO EM MESES])
 
 -- Cadastrar as receitas do usuário. 
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (1, 100.00, TO_DATE('10/10/2023', 'DD/MM/YYYY'), 'Prestação de serviço - banco de dados', 1, 1, 1);
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (2, 1829.22, TO_DATE('10/10/2023', 'DD/MM/YYYY'), 'Prestação de Serviço - desenvolvimento de sistemas', 1, 1, 1);
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (3, 2437.55, TO_DATE('30/09/2023', 'DD/MM/YYYY'), 'Parcela do imóvel', 1, 1, 2);
+INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) VALUES ([cd_lancamento], [VALOR LANCAMENTO], TO_DATE('[DATA DO LANCAMENTO]', 'DD/MM/YYYY'), '[DESCRICAO DO LANCAMENTO]', [CD TIPO RECEITA], [CD USUARIO], [CD CONTA]);
 
 -- Alterar todos os dados das receitas do usuário, utilizando o código como referência. 
-UPDATE T_LANCAMENTO SET vl_lancamento = 1000.00 WHERE cd_lancamento = 1;
-UPDATE T_LANCAMENTO SET vl_lancamento = 8534.94 WHERE cd_lancamento = 2;
+UPDATE T_LANCAMENTO SET vl_lancamento = [VALOR LANCAMENTO] WHERE cd_lancamento = [CD LANCAMENTO];
 
 -- Cadastrar as despesas do usuário. 
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (4, 185.32, TO_DATE('01/10/2023', 'DD/MM/YYYY'), 'Conta de Luz', 2, 1, 1);
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (5, 1829.22, TO_DATE('10/10/2023', 'DD/MM/YYYY'), 'Fatura de Cartão de Crédito', 2, 1, 1);
-INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) 
-VALUES (6, 2437.55, TO_DATE('30/09/2023', 'DD/MM/YYYY'), 'Parcela do imóvel', 2, 1, 2);
+INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta) VALUES ([cd_lancamento], [VALOR LANCAMENTO], TO_DATE('[DATA DO LANCAMENTO]', 'DD/MM/YYYY'), '[DESCRICAO DO LANCAMENTO]', [CD TIPO DESPESA], [CD USUARIO], [CD CONTA]);
 
 -- Alterar todos os dados das despesas do usuário, utilizando o código como referência. 
-
+UPDATE T_LANCAMENTO SET vl_lancamento = [VALOR LANCAMENTO] WHERE cd_lancamento = [CD LANCAMENTO];
 
 -- Cadastrar os dados para investimentos. 
-
+INSERT INTO T_LANCAMENTO(cd_lancamento, vl_lancamento, dt_lancamento, ds_lancamento, cd_tipo, cd_usuario, cd_conta, cd_tipo_investimento) VALUES ([cd_lancamento], [VALOR LANCAMENTO], TO_DATE('[DATA DO LANCAMENTO]', 'DD/MM/YYYY'), '[DESCRICAO DO LANCAMENTO]', [CD TIPO INVESTIMENTO], [CD USUARIO], [CD CONTA], [CD TIPO CARTEIRA INVESTIMENTO]);
 
 -- Alterar todos os dados para investimentos do usuário, utilizando o código como referência. 
-
+UPDATE T_LANCAMENTO SET vl_lancamento = [VALOR LANCAMENTO], cd_tipo_investimento = [CD TIPO CARTEIRA INVESTIMENTO] WHERE cd_lancamento = [CD LANCAMENTO];
 
 
 /* 
@@ -52,27 +41,38 @@ VALUES (6, 2437.55, TO_DATE('30/09/2023', 'DD/MM/YYYY'), 'Parcela do imóvel', 2
 */
 
 -- Consultar os dados de um usuário (filtrar a partir do seu código). 
+SELECT * FROM T_USUARIO WHERE cd_usuario = [CD USUARIO];
 
-
--- Consultar os dados de um único registro de despesa de um  usuário 
+-- Consultar os dados de um único registro de despesa de um usuário 
 -- (filtrar a partir do código do usuário e do código da despesa). 
-
+SELECT * FROM T_LANCAMENTO WHERE cd_usuario = [CD USUARIO] AND cd_lancamento = [CD LANCAMENTO];
 
 -- Consultar os dados de todos os registros de despesas de um  usuário, 
 -- ordenando-os dos registros mais recentes para os mais antigos 
--- (filtrar a partir do seu código). */
-
+-- (filtrar a partir do seu código).
+SELECT * FROM T_LANCAMENTO WHERE cd_usuario = [CD USUARIO] ORDER BY dt_lancamento DESC;
 
 -- Consultar os dados de um único registro de investimento de um  usuário 
--- (filtrar a partir do código do usuário e do código de investimento. */
-
+-- (filtrar a partir do código do usuário e do código de investimento.
+SELECT * FROM T_LANCAMENTO WHERE cd_usuario = [CD USUARIO] AND cd_lancamento = [CD LANCAMENTO];
 
 -- Consultar os dados de todos os registros de investimentos de um  usuário, 
 -- ordenando-os dos registros mais recentes para os mais antigos 
--- (filtrar a partir do seu código). */
-
+-- (filtrar a partir do seu código).
+SELECT * FROM T_LANCAMENTO WHERE cd_usuario = [CD USUARIO] AND cd_tipo = [CD TIPO INVESTIMENTO];
 
 -- Consultar os dados básicos de um usuário, o último investimento registrado 
 -- e a última despesa registrada (filtrar a partir do código de 
 -- usuário – consulta necessária para o dashboard. Dica: veja consulta 
 -- com junções). 
+SELECT 
+  u.nm_usuario, 
+  i.vl_lancamento AS vl_investimento, 
+  MAX(i.dt_lancamento) AS dt_investimento,
+  d.vl_lancamento AS vl_despesa, 
+  MAX(d.dt_lancamento) AS dt_despesa
+FROM T_USUARIO u
+LEFT JOIN T_LANCAMENTO d ON d.cd_usuario = u.cd_usuario and d.cd_tipo = [CD TIPO DESPESA]
+LEFT JOIN T_LANCAMENTO i ON d.cd_usuario = u.cd_usuario and d.cd_tipo = [CD TIPO INVESTIMENTO]
+LEFT JOIN T_TIPO_INVESTIMENTO ti ON i.cd_tipo_investimento = ti.cd_tipo_investimento
+WHERE cd_usuario = [CD USUARIO];
